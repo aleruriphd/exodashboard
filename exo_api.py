f9127e59209849edecb9cad5f066821064785fd5
aleruriphd <aleruri@gmail.com>
2024-03-12 11:47:56 +1000

    Uncommented sns import

--- exo_api.py
+++ exo_api.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 from ipywidgets import interact
